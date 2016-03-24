ba4e977cfd2cfed097b884342a523fdac05e2d53
jiawenqiang <jyq_51357@163.com>
2016-03-24 16:11:37 +0800

    Initial Commit

---
+++ HJQRCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HJQRCode
+//
+//  Created by 贾文强 on 16/3/24.
+//  Copyright © 2016年 贾文强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
