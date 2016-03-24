41923b9172c3209e5b8afb4153c47b5de252e25c
jiawenqiang <jyq_51357@163.com>
2016-03-24 16:20:04 +0800

    init

--- HJQRCode/ViewController.swift
+++ HJQRCode/ViewController.swift
@@ -1,25 +1,91 @@
-//
-//  ViewController.swift
-//  HJQRCode
-//
-//  Created by 贾文强 on 16/3/24.
-//  Copyright © 2016年 贾文强. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+// 1.引入AVFoundation框架
+import AVFoundation
 
+// 2.声明代理
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    // 3.声明变量
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    let labIntroudction = UILabel(frame:CGRectMake(15, 80, 290, 50))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 4.实例化AVCaptureSession对象
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        var error:NSError?
+        let input:AnyObject!
+        do {
+            input = try AVCaptureDeviceInput(device: captureDevice)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if(error != nil) {
+            print("\(error?.localizedDescription)")
+            return
+        }
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(input as! AVCaptureInput)
+        
+        // 5.翻译获取图像，输出
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        // 6.绘制捕获界面
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        // --绘制取景框
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubviewToFront(qrCodeFrameView!)
+        
+        // --
+        labIntroudction.backgroundColor = UIColor.clearColor()
+        labIntroudction.numberOfLines = 2
+        labIntroudction.textColor = UIColor.redColor()
+        self.view.addSubview(labIntroudction)
+        
+        // 7.启动会话，开始捕获
+        captureSession?.startRunning()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // 8.解析二维码：识别后的回调方法
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0{
+            qrCodeFrameView?.frame = CGRectZero
+            return
+        }
+        
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode{
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barCodeObject.bounds
+            
+            // 9.解析结果
+            if metadataObj.stringValue != nil{
+                self.labIntroudction.text = "识别结果" + metadataObj.stringValue
+                return
+            }
+        }
+    }
 }
-
